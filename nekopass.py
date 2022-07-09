6b2215fec0440874c90ef451498b194f6c36bfc1
W. Santos <w.santos.gg@gmail.com>
2022-07-09 19:59:56 +0800

    finally changed nekopass.py

---
+++ nekopass.py
@@ -0,0 +1,39 @@
+import src.sql as sql
+from getpass import getpass
+
+if __name__ == '__main__':
+    print('hello user')
+
+    print(' 1.) Insert Password')
+    print(' 2.) Get Accounts')
+    print(' 3.) Get Password')
+    print(' 4.) Quit')
+
+    while True:
+        command = int(input('>'))
+
+        if command == 1:
+            account = input('account name >')
+            password = getpass('password >')
+            nickname = input('nickname (default will be \'\') >')
+
+            if account == '':
+                print('account must not be null')
+                continue
+
+            if password == '':
+                print('password must not be null')
+                continue
+            
+            sql.add_password(account, password, nickname)
+            
+        if command == 2:
+            continue
+
+        if command == 3:
+            continue
+
+        if command == 4:
+            print('Goodbye!')
+            exit()
+            
